e6b9d333564f0158111709c83a014798abe63ed0
aatlove01 <aaa1150@nyu.edu>
2022-05-18 12:41:44 -0400

    Shopify

---
+++ ShopifyChallenge/aiResponses.js
@@ -0,0 +1,7 @@
+console.log("javascript is here")
+
+const submit = document.getElementById('submitButton');
+const textvalue = document.getElementById('textbox');
+
+submit.addEventListener('click',values);
+console.log('going to values function')
