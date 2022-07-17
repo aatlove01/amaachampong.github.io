7b96ce06520f1d9944de764974b1062bf4180ffe
aatlove01 <aaa1150@nyu.edu>
2022-07-17 16:21:31 -0400

    Save

--- ShopifyChallenge/aiResponses.js
+++ ShopifyChallenge/aiResponses.js
@@ -2,6 +2,17 @@ console.log("javascript is here")
 
 const submit = document.getElementById('submitButton');
 const textvalue = document.getElementById('textbox');
+const behindText = document.getElementById('box');
 
-submit.addEventListener('click',values);
+submit.addEventListener('click', values)
 console.log('going to values function')
+
+function values(){
+    const valueList = textvalue.value.split(" ");
+    console.log('collect value');
+    console.log(valueList);
+    behindText.style.right = '10vh';
+    behindText.style.top = '15vh';
+    behindText.style.display = 'block';
+    
+}
